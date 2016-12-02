1ee054189c2717cd13951d4d7a53c2b1ceec203f
Matthew Carter <m@ahungry.com>
2016-12-01 23:58:23 -0500

    Save excursion to prevent losing place in refresh issue

--- org-jira.el
+++ org-jira.el
@@ -521,6 +521,11 @@ With a prefix argument, allow you to customize the jql.  See
 (defun org-jira-get-issue-summary (issue)
   (org-jira-find-value issue 'fields 'summary))
 
+(defun org-jira-get-issue-list-callback (&rest data &allow-other-keys)
+  "Callback for async, DATA is the response from the request call."
+  (let ((issues (append (cdr (assoc 'issues (getf data :data))) nil)))
+    (org-jira-get-issues issues)))
+
 (defun org-jira-get-issues (issues)
   "Get list of ISSUES into an org buffer.
 
@@ -530,10 +535,7 @@ See`org-jira-get-issue-list'"
   ;; If the user doesn't provide a default, async call to build an issue list
   ;; from the JQL style query
   (interactive
-   (org-jira-get-issue-list
-    (lambda (&rest data &allow-other-keys)
-      (let ((issues (append (cdr (assoc 'issues (getf data :data))) nil)))
-        (org-jira-get-issues issues)))))
+   (org-jira-get-issue-list 'org-jira-get-issue-list-callback))
   (let (project-buffer)
     (mapc (lambda (issue)
             (let* ((proj-key (org-jira-get-issue-project issue))
@@ -544,71 +546,72 @@ See`org-jira-get-issue-list'"
                 (setq project-buffer (or (find-buffer-visiting project-file)
                                          (find-file project-file)))
                 (with-current-buffer project-buffer
-                  (org-jira-mode t)
-                  (widen)
-                  (show-all)
-                  (goto-char (point-min))
-                  (setq p (org-find-entry-with-id issue-id))
-                  (save-restriction
-                    (if (and p (>= p (point-min))
-                             (<= p (point-max)))
-                        (progn
-                          (goto-char p)
-                          (forward-thing 'whitespace)
-                          (kill-line))
-                      (goto-char (point-max))
-                      (unless (looking-at "^")
-                        (insert "\n"))
-                      (insert "* "))
-                    (let ((status (org-jira-get-issue-val 'status issue)))
-                      (org-jira-insert (concat (cond (org-jira-use-status-as-todo
-                                                      (upcase (replace-regexp-in-string " " "-" status)))
-                                                     ((member status org-jira-done-states) "DONE")
-                                                     ("TODO")) " "
-                                                     issue-headline)))
-                    (save-excursion
-                      (unless (search-forward "\n" (point-max) 1)
-                        (insert "\n")))
-                    (org-narrow-to-subtree)
-                    (org-change-tag-in-region
-                     (point-min)
-                     (save-excursion
-                       (forward-line 1)
-                       (point))
-                     (replace-regexp-in-string "-" "_" issue-id)
-                     nil)
+                  (save-excursion
+                    (org-jira-mode t)
+                    (widen)
+                    (show-all)
+                    (goto-char (point-min))
+                    (setq p (org-find-entry-with-id issue-id))
+                    (save-restriction
+                      (if (and p (>= p (point-min))
+                               (<= p (point-max)))
+                          (progn
+                            (goto-char p)
+                            (forward-thing 'whitespace)
+                            (kill-line))
+                        (goto-char (point-max))
+                        (unless (looking-at "^")
+                          (insert "\n"))
+                        (insert "* "))
+                      (let ((status (org-jira-get-issue-val 'status issue)))
+                        (org-jira-insert (concat (cond (org-jira-use-status-as-todo
+                                                        (upcase (replace-regexp-in-string " " "-" status)))
+                                                       ((member status org-jira-done-states) "DONE")
+                                                       ("TODO")) " "
+                                                       issue-headline)))
+                      (save-excursion
+                        (unless (search-forward "\n" (point-max) 1)
+                          (insert "\n")))
+                      (org-narrow-to-subtree)
+                      (org-change-tag-in-region
+                       (point-min)
+                       (save-excursion
+                         (forward-line 1)
+                         (point))
+                       (replace-regexp-in-string "-" "_" issue-id)
+                       nil)
 
-                    (mapc (lambda (entry)
-                            (let ((val (org-jira-get-issue-val entry issue)))
-                              (when (and val (not (string= val "")))
-                                (org-entry-put (point) (symbol-name entry) val))))
-                          '(assignee reporter type priority resolution status components created updated))
-                    (org-entry-put (point) "ID" (org-jira-get-issue-key issue))
+                      (mapc (lambda (entry)
+                              (let ((val (org-jira-get-issue-val entry issue)))
+                                (when (and val (not (string= val "")))
+                                  (org-entry-put (point) (symbol-name entry) val))))
+                            '(assignee reporter type priority resolution status components created updated))
+                      (org-entry-put (point) "ID" (org-jira-get-issue-key issue))
 
-                    (mapc (lambda (heading-entry)
-                            (ensure-on-issue-id
-                             issue-id
-                             (let* ((entry-heading (concat (symbol-name heading-entry) (format ": [[%s][%s]]" (concat jiralib-url "/browse/" issue-id) issue-id))))
-                               (setq p (org-find-exact-headline-in-buffer entry-heading))
-                               (if (and p (>= p (point-min))
-                                        (<= p (point-max)))
-                                   (progn
-                                     (goto-char p)
-                                     (org-narrow-to-subtree)
-                                     (goto-char (point-min))
-                                     (forward-line 1)
-                                     (delete-region (point) (point-max)))
-                                 (if (org-goto-first-child)
-                                     (org-insert-heading)
-                                   (goto-char (point-max))
-                                   (org-insert-subheading t))
-                                 (org-jira-insert entry-heading "\n"))
+                      (mapc (lambda (heading-entry)
+                              (ensure-on-issue-id
+                               issue-id
+                               (let* ((entry-heading (concat (symbol-name heading-entry) (format ": [[%s][%s]]" (concat jiralib-url "/browse/" issue-id) issue-id))))
+                                 (setq p (org-find-exact-headline-in-buffer entry-heading))
+                                 (if (and p (>= p (point-min))
+                                          (<= p (point-max)))
+                                     (progn
+                                       (goto-char p)
+                                       (org-narrow-to-subtree)
+                                       (goto-char (point-min))
+                                       (forward-line 1)
+                                       (delete-region (point) (point-max)))
+                                   (if (org-goto-first-child)
+                                       (org-insert-heading)
+                                     (goto-char (point-max))
+                                     (org-insert-subheading t))
+                                   (org-jira-insert entry-heading "\n"))
 
-                               (org-jira-insert (replace-regexp-in-string "^" "  " (org-jira-get-issue-val heading-entry issue))))))
-                          '(description))
-                    (org-jira-update-comments-for-current-issue)
-                    (org-jira-update-worklogs-for-current-issue)
-                    )))))
+                                 (org-jira-insert (replace-regexp-in-string "^" "  " (org-jira-get-issue-val heading-entry issue))))))
+                            '(description))
+                      (org-jira-update-comments-for-current-issue)
+                      (org-jira-update-worklogs-for-current-issue)
+                      ))))))
           issues)
     (switch-to-buffer project-buffer)))
 
